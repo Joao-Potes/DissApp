72dcb0ecbf5e16c010ecc95e7033766bf994d2b9
Joao-Potes <4fragj68@solent.ac.uk>
2024-02-24 02:35:44 +0000

    Simple header and pages template created

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 8000;
 
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('index'));
+app.set("view engine", "ejs");
+app.get("/index", (req, res) => res.render("index.ejs"));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/contact", (req, res) => res.render("contact"));
+app.get("/policy", (req, res) => res.render("policy"));
+app.get("/feedback", (req, res) => res.render("feedback"));
 
-
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
-
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
