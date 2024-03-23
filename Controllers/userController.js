898ef136cfabc05523ced95681abbfe75229a872
Joao-Potes <4fragj68@solent.ac.uk>
2024-03-23 12:58:34 +0000

    user and register page created, user controllers and routes implemented to handle pages forms and data

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,5 @@
 // controllers/userController.js
-const User = require('../Models/user');
+const User = require("../Models/user");
 
 exports.createUser = async (req, res) => {
   try {
@@ -9,7 +9,34 @@ exports.createUser = async (req, res) => {
       password: req.body.password,
     });
     res.status(201).send(newUser);
+    res.redirect('/login')
   } catch (error) {
     res.status(400).send(error);
-    }
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // check if all fields are provided
+    if (!req.body.email || !req.body.password)
+      return res.status(400).json({ message: "Email and password required" });
+    const email = req.body.email.toLowerCase();
+    // check if user exists
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    // check if password matches
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    res.redirect('/')
+
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
--- router/userRoute.js
+++ router/userRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../Controllers/userController');
 
 router.post('/register', userController.createUser);
+router.post('/login', userController.login);
 
 module.exports = router;
